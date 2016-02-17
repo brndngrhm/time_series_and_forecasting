d70ee1f02f198241a57f1297975cf37680e3a05b
brndngrhm <brendanvgraham@gmail.com>
2016-02-17 16:02:00 -0500

    adds 2nd acf plot set

--- week_5/hw_5.R
+++ week_5/hw_5.R
@@ -1,6 +1,6 @@
 #hw 5
 
-#acf for a
+#acf for a----
 
 a <- ARMAacf(ar=c(-0.2, 0.48), ma = 1, lag.max=50)
 plot(0:50,a,type='h',xlab='Lag')
@@ -15,7 +15,32 @@ plot(0:50,c,type='h',xlab='Lag')
 d <- ARMAacf(ar=c(-1.8, -0.81), ma=c(1),lag.max=50)
 plot(0:50,d,type='h',xlab='Lag')
 
-#plot them all
+#plot them all as they are on hw sheet
+par(mfrow = c(3,1))
+plot(0:50,a,type='h',xlab='Lag', main = "ACF of Part (a)")
+plot(0:50,c,type='h',xlab='Lag', main = "ACF of Part (c)")
+plot(0:50,d,type='h',xlab='Lag', main = "ACF of Part (d)")
+
+dev.off()
+
+#alt acf plots----
+
+#acf for a
+
+a <- ARMAacf(ar=c(1, 0.2, -0.48), ma = 1, lag.max=50)
+plot(0:50,a,type='h',xlab='Lag')
+
+#acf for c
+
+c <- ARMAacf(ar=c(1, 0.6), ma=c(1, 1.2),lag.max=50)
+plot(0:50,c,type='h',xlab='Lag')
+
+#acf for d
+
+d <- ARMAacf(ar=c(1, 1.8, 0.81), ma=c(1),lag.max=50)
+plot(0:50,d,type='h',xlab='Lag')
+
+#plot them all as they are in the fucntions theta(Z) and phi(z)
 par(mfrow = c(3,1))
 plot(0:50,a,type='h',xlab='Lag', main = "ACF of Part (a)")
 plot(0:50,c,type='h',xlab='Lag', main = "ACF of Part (c)")
