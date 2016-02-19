3d65b62272d8351aa5328a132628b41ed168c7d9
brndngrhm <brendanvgraham@gmail.com>
2016-02-19 11:06:08 -0500

    small fixes

--- week_5/hw_5.R
+++ week_5/hw_5.R
@@ -1,18 +1,15 @@
 #hw 5
 
 #acf for a----
-
-a <- ARMAacf(ar=c(-0.2, 0.48), ma = 1, lag.max=50)
+a <- ARMAacf(ar=c(-0.2, 0.48), ma=1,lag.max=50)
 plot(0:50,a,type='h',xlab='Lag')
 
 #acf for c
-
 c <- ARMAacf(ar=c(-0.6), ma=c(1, 1.2),lag.max=50)
 plot(0:50,c,type='h',xlab='Lag')
 
 #acf for d
-
-d <- ARMAacf(ar=c(-1.8, -0.81), ma=c(1),lag.max=50)
+d <- ARMAacf(ar=c(-1.8, -0.81), ma=1,lag.max=50)
 plot(0:50,d,type='h',xlab='Lag')
 
 #plot them all as they are on hw sheet
