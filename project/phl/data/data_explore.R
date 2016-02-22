f2da06e39ae30c2745195d25a62720d664f3c042
brndngrhm <brendanvgraham@gmail.com>
2016-02-21 19:19:46 -0500

    fixed plots

--- project/phl/data/data_explore.R
+++ project/phl/data/data_explore.R
@@ -2,6 +2,7 @@
 
 library(ggplot2)
 library(dplyr)
+library(scales)
 
 load("~/R Working Directory/Villanova/time_series_and_forecasting/project/phl/data/phl.rda")
 
@@ -11,11 +12,16 @@ plot(phl.ts, type = "o")
 
 #some plots
 
-pax.plot <- ggplot(phl, aes(x=date, y=pax)) + 
+(pax.plot <- ggplot(phl, aes(x=date, y=pax)) + 
   geom_point() + geom_line() + 
-  scale_x_datetime(breaks = date_breaks("1 year"), minor_breaks = date_breaks("1 month")) + 
-  labs(x="/nDate", y="Enplanements/n", title = "PHL Monthly Enplanements: 2007-2015\n")
+  labs(x= "", y= "Enplanements\n", title = "PHL Monthly Enplanements: 2007-2015\n") + 
+  scale_x_datetime(breaks = date_breaks("1 year")))
 
-(emp.plot <- ggplot(phl, aes(x=date, y=emp)) + geom_point() + geom_line() + scale_x_datetime(breaks = date_breaks("1 year")))
+(emp.plot <- ggplot(phl, aes(x=date, y=emp)) + 
+  geom_point() + geom_line() + 
+  labs(x= "", y= "Employment\n", title = "PHL Monthly Employment: 2007-2015\n") + 
+  scale_x_datetime(breaks = date_breaks("1 year")))
 
-(earn.plot <- ggplot(phl, aes(x=date, y=earnings)) + geom_point() + geom_line() + scale_x_datetime(breaks = date_breaks("1 year")))
+(earn.plot <- ggplot(phl, aes(x=date, y=earnings)) + geom_point() + geom_line() + 
+  labs(x= "", y= "Earnings\n", title = "PHL Monthly Earnings: 2007-2015\n") + 
+  scale_x_datetime(breaks = date_breaks("1 year")))
