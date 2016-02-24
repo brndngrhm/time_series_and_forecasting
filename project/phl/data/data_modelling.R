53f38d59fec4b03b545699fef3aa6179fa1edb6d
brndngrhm <brendanvgraham@gmail.com>
2016-02-24 11:57:40 -0500

    modelling script

---
+++ project/phl/data/data_modelling.R
@@ -0,0 +1,20 @@
+#modelling
+
+#packages ----
+library(ggplot2)
+library(dplyr)
+library(scales)
+library(astsa)
+library(ggthemes)
+library(extrafont)
+
+#load data and exploration script----
+#for work
+load("C:/Users/GRA/Desktop/Misc/R Working Directory/School/time_series_and_forecasting/project/phl/data/phl.rda")
+
+#for home
+#load("~/R Working Directory/Villanova/time_series_and_forecasting/project/phl/data/phl.rda")
+
+source("C:/Users/GRA/Desktop/Misc/R Working Directory/School/time_series_and_forecasting/project/phl/data/data_explore.R")
+
+#
