205d08948964949f95cb018e09504509a62a1301
brndngrhm <brendanvgraham@gmail.com>
2016-04-21 11:37:05 -0400

    added actual phl pax to plot

--- project/phl/scripts/sarima_for_pax.R
+++ project/phl/scripts/sarima_for_pax.R
@@ -24,23 +24,27 @@ acf2(resid(model$fit)^2) #pulling out and plotting squared residuals
 #SARIMA forecast
 sarima.for(log(pax), 2, 2, 1, 3, 0, 1, 1, 12) #next 2 months forecast are 13.83949 and 13.92725
 
+#getting actual phl pax for new months
+x.phl.new <- getURL("https://raw.githubusercontent.com/brndngrhm/time_series_and_forecasting/master/project/phl/data/phl_new.csv")
+phl.new <- read.csv(text = x.phl.new)
+names(phl.new) <- tolower(names(phl.new))
+phl.new$month <- as.numeric(phl.new$month)
+phl.new$origin <- as.character(phl.new$origin)
+phl.new <- phl.new %>% dplyr::filter(origin == "PHL" & month > 8) %>% group_by(month) %>% summarise(pax = sum(passengers)) %>% ungroup()
+
 #getting fitted values and plotting with original points and adding forecast looks good
 fit <- ts(fitted(arima(log(pax), order=c(2, 1, 3), seasonal = c(0, 1, 1))), frequency = 12)
 time <- seq(1, 104)
 forecast.time <- seq(104, 106)
 forecast.points <- c(14.0242, 13.83949, 13.92725)
-actual.points<- 
+actual.points <- c(14.0242, 13.89314, 13.97053)
 plot(time, log(pax), type = "p", xlim=c(0, 106))
 lines(time, fit, col="red", type ="l")
 points(105,13.83949, col="blue", pch=24)
 points(106, 13.92725, col="blue", pch=24)
 lines(forecast.time, forecast.points, type="l", col="blue")
+points(105, 13.89314, col="green")
+points(106, 13.97053, col="green")
+lines(forecast.time, actual.points, type="l", col="green")
 
 
-#phl.new <- read.csv("~/R Working Directory/Villanova/time_series_and_forecasting/project/phl/data/phl_new.csv")
-phl.new <- read.csv("C:/Users/GRA/Documents/Brendan Graham/R Working Directory/Villanova/time_series_and_forecasting/project/phl/data/phl_new_laptop.csv")
-
-names(phl.new) <- tolower(names(phl.new))
-phl.new$month <- as.numeric(phl.new$month)
-phl.new$origin <- as.character(phl.new$origin)
-phl.new <- phl.new %>% dplyr::filter(origin == "PHL" & month > 8) %>% group_by(month) %>% summarise(pax = sum(passengers)) %>% ungroup()
