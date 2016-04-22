92bc8da005f8ea5c64777daee5447e14e8a30133
brndngrhm <brendanvgraham@gmail.com>
2016-04-22 09:53:59 -0400

    added to plots

--- project/phl/scripts/sarima_for_pax.R
+++ project/phl/scripts/sarima_for_pax.R
@@ -24,6 +24,8 @@ acf2(resid(model$fit)^2) #pulling out and plotting squared residuals
 #SARIMA forecast
 sarima.for(log(pax), 2, 2, 1, 3, 0, 1, 1, 12) #next 2 months forecast are 13.83949 and 13.92725
 
+
+
 #getting actual phl pax for new months
 x.phl.new <- getURL("https://raw.githubusercontent.com/brndngrhm/time_series_and_forecasting/master/project/phl/data/phl_new.csv")
 phl.new <- read.csv(text = x.phl.new)
@@ -32,18 +34,48 @@ phl.new$month <- as.numeric(phl.new$month)
 phl.new$origin <- as.character(phl.new$origin)
 phl.new <- phl.new %>% dplyr::filter(origin == "PHL" & month > 8) %>% group_by(month) %>% summarise(pax = sum(passengers)) %>% ungroup()
 
-#getting fitted values and plotting with original points and adding forecast looks good
-fit <- ts(fitted(arima(log(pax), order=c(2, 1, 3), seasonal = c(0, 1, 1))), frequency = 12)
-time <- seq(1, 104)
-forecast.time <- seq(104, 106)
-forecast.points <- c(14.0242, 13.83949, 13.92725)
-actual.points <- c(14.0242, 13.89314, 13.97053)
 
-plot(time, log(pax), type = "p", xlim=c(0, 106))
-lines(time, fit, col="red", type ="l")
-points(105,13.83949, col="blue", pch=24)
-points(106, 13.92725, col="blue", pch=24)
-lines(forecast.time, forecast.points, type="l", col="blue")
-points(105, 13.89314, col="green")
-points(106, 13.97053, col="green")
-lines(forecast.time, actual.points, type="l", col="green")
+#getting fitted values and plotting with original points and adding forecast looks good
+
+#sarima fit
+sarima.fit <- ts(fitted(arima(log(pax), order=c(2, 1, 3), seasonal = c(0, 1, 1))), frequency = 12)
+
+#decomp fit
+decomp.fit <- out$fitted.values
+
+#stuff for plot
+time <- seq(1, 104)
+time.decomp <- seq(1:96)
+time.actual <- seq(1:106)
+forecast.time <- seq(104, 106)
+actual.pax <- c(14.0242, 13.89314, 13.97053)
+
+#various predicted values from models
+sarima.pred <- c(14.0242, 13.83949, 13.92725)
+decomp.pred <- c(14.0242, 13.92797, 13.83861)
+
+#plot
+plot(time, log(pax), type = "p", xlim=c(80, 108))
+abline(v=time[104])
+
+#adding SARIMA fitted line
+lines(time, sarima.fit, col="red", type ="l")
+
+#adding decomp fitted line
+lines(time.decomp, decomp.fit, col="darkgreen", type="l")
+
+#adding actual Pax
+points(105, 13.89314, col="deepskyblue2")
+points(106, 13.97053, col="deepskyblue2")
+lines(forecast.time, actual.pax, type="l", col="deepskyblue2", lwd=2)
+
+#SARIMA predictions
+points(105,13.83949, col="green3", pch=24)
+points(106, 13.92725, col="green3", pch=24)
+lines(forecast.time, sarima.pred, type="l", col="green3", lwd=2)
+
+#Decomp predictions
+points(105,13.92797, col="darkgreen", pch=24)
+points(106, 13.83861, col="darkgreen", pch=24)
+lines(forecast.time, decomp.pred, type="l", col="darkgreen", lwd=2)
+
