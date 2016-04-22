75fbd5e655b2e11b745e27321727c665614e19e1
brndngrhm <brendanvgraham@gmail.com>
2016-04-22 11:03:53 -0400

    more plot stuff

--- project/phl/scripts/sarima_for_pax.R
+++ project/phl/scripts/sarima_for_pax.R
@@ -43,17 +43,21 @@ sarima.fit <- ts(fitted(arima(log(pax), order=c(2, 1, 3), seasonal = c(0, 1, 1))
 #decomp fit
 decomp.fit <- out$fitted.values
 
+#reg.arma fit
+reg.arma.fit <- fitted(fit)
+
 #stuff for plot
 time <- seq(1, 104)
 time.decomp <- seq(1:96)
 time.actual <- seq(1:106)
+time.reg.arma <-  seq(1:103)
 forecast.time <- seq(104, 106)
 actual.pax <- c(14.0242, 13.89314, 13.97053)
 
 #various predicted values from models
-sarima.pred <- c(14.0242, 13.83949, 13.92725)
+sarima.pred <- c(14.01201, 13.83949, 13.92725)
 decomp.pred <- c(14.0242, 13.97036, 13.93802)
-reg.pred <- c(14.0242, 13.80375, 13.76140)
+reg.pred <- c(13.95143, 13.80375, 13.76140)
 
 #plot
 plot(time, log(pax), type = "p", xlim=c(80, 108))
@@ -65,23 +69,26 @@ lines(time, sarima.fit, col="red", type ="l")
 #adding decomp fitted line
 lines(time.decomp, decomp.fit, col="darkgreen", type="l")
 
+#adding reg.arma fitted line
+lines(time.reg.arma, reg.arma.fit, col="mediumpurple3", type="l")
+
 #adding actual Pax
 points(105, 13.89314, col="deepskyblue2")
 points(106, 13.97053, col="deepskyblue2")
 lines(forecast.time, actual.pax, type="l", col="deepskyblue2", lwd=2)
 
 #SARIMA predictions
-points(105,13.83949, col="red", pch=24)
+points(105, 13.83949, col="red", pch=24)
 points(106, 13.92725, col="red", pch=24)
 lines(forecast.time, sarima.pred, type="l", col="red", lwd=2)
 
 #Decomp predictions
-points(105,13.97036, col="darkgreen", pch=24)
+points(105, 13.97036, col="darkgreen", pch=24)
 points(106, 13.93802, col="darkgreen", pch=24)
 lines(forecast.time, decomp.pred, type="l", col="darkgreen", lwd=2)
 
-#Reg w/ arma errors predictions
-points(105,13.80375, col="mediumpurple3", pch=24)
+#Reg w/ arma errors prediction
+points(105, 13.80375, col="mediumpurple3", pch=24)
 points(106, 13.76140, col="mediumpurple3", pch=24)
 lines(forecast.time, reg.pred, type="l", col="mediumpurple3", lwd=2)
 
